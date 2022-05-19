e23c76d0267e53628f5c6c8371dcfcbf2b0c0640
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-05-19 05:09:14 +0100

    Update tests, again

--- tests/test_read.py
+++ tests/test_read.py
@@ -3,10 +3,12 @@ def test_get_header(data_path, data_comment_path):
 
     header, nlines = read_header(data_path)
     assert isinstance(header, dict)
+    assert "freq" in header.keys()
     assert nlines == 18
 
     header2, nlines = read_header(data_comment_path, "# ")
     assert isinstance(header2, dict)
+    assert "freq" in header.keys()
     assert nlines == 18
 
     assert header == header2
