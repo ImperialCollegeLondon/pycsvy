1c410c3c46e86a20e5e34d6c4d22f18c663cae48
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-05-19 22:42:29 +0100

    Improve __init__

--- csvy/__init__.py
+++ csvy/__init__.py
@@ -1,3 +1,6 @@
+"""
+Python reader/writer for CSV files with YAML header information.
+"""
 __version__ = "0.1.3"
-from .readers import read_to_array, read_to_dataframe
-from .writers import write
+from .readers import read_to_array, read_to_dataframe  # noqa: F401
+from .writers import write  # noqa: F401
