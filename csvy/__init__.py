48f8a9f24fa7b40a05e6aaaa280ec0cb20e32ada
Alex Dewar <a.dewar@imperial.ac.uk>
2023-01-04 16:48:16 +0000

    Export read_header function
    It could be useful outside the submodule.

--- csvy/__init__.py
+++ csvy/__init__.py
@@ -2,5 +2,5 @@
 Python reader/writer for CSV files with YAML header information.
 """
 __version__ = "0.2.0"
-from .readers import read_to_array, read_to_dataframe  # noqa: F401
+from .readers import read_header, read_to_array, read_to_dataframe  # noqa: F401
 from .writers import write  # noqa: F401
