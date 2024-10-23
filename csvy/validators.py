73823faf220adb9258fa3de52f0e66c3b1a07d2e
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-10-23 17:05:51 +0100

    :sparkles: Add column validator and associated classes.

--- csvy/validators.py
+++ csvy/validators.py
@@ -1,8 +1,11 @@
 """Module that contains validators for the CSVY file format."""
 
+from __future__ import annotations
+
 import csv
 from collections.abc import Mapping
-from typing import Any, Callable, Optional, TypeVar
+from enum import Enum
+from typing import Any, Callable, TypeVar
 
 from pydantic import BaseModel, Field
 
@@ -95,6 +98,8 @@ def header_to_dict(header: dict[str, Any]) -> dict[str, Any]:
     return validated_header
 
 
+# CSV Dialect-related validation
+
 # Create a generic variable that can be 'Parent', or any subclass.
 T = TypeVar("T", bound="CSVDialectValidator")
 
@@ -127,7 +132,7 @@ class CSVDialectValidator(BaseModel):
 
     delimiter: str = Field(default=",")
     doublequote: bool = Field(default=True)
-    escapechar: Optional[str] = Field(default=None)
+    escapechar: str | None = Field(default=None)
     lineterminator: str = Field(default="\r\n")
     quotechar: str = Field(default='"')
     skipinitialspace: bool = Field(default=False)
@@ -223,3 +228,113 @@ class CSVDialectValidator(BaseModel):
             quotechar=unix.quotechar or '"',
             skipinitialspace=unix.skipinitialspace,
         )
+
+
+# Table Schema-related validation
+
+
+class TypeEnum(str, Enum):
+    """Enumeration of the possible types for the Table Schema."""
+
+    STRING = "string"
+    INTEGER = "integer"
+    BOOLEAN = "boolean"
+    OBJECT = "object"
+    ARRAY = "array"
+    DATE = "date"
+    TIME = "time"
+    DATETIME = "datetime"
+    YEAR = "year"
+    YEARMONTH = "yearmonth"
+    DURATION = "duration"
+    GEOPOINT = "geopoint"
+    GEOJSON = "geojson"
+    ANY = "any"
+
+
+class ConstraintsValidator(BaseModel):
+    """Validator for the constraints in the Table Schema.
+
+    This class is used to validate the constraints in the Table Schema. It is based on
+    the constraints defined in the Table Schema specification.
+
+    Attributes:
+        required: A boolean indicating if the value is required.
+        unique: A boolean indicating if the value is unique.
+        minimum: The minimum of the value. Applies to types: integer, number, date,
+            time, datetime, year, yearmonth.
+        maximum: The maximum value. Applies to types: integer, number, date,
+            time, datetime, year, yearmonth.
+        minLength: The minimum length of the field. Applies to collections (string,
+            array, object).
+        maxLength: The maximum length of the field. Applies to collections (string,
+            array, object).
+        pattern: A regular expression pattern that the value must match. Applies to
+            types: string.
+        enum: A list of possible values for the field.
+
+    """
+
+    required: bool | None = Field(None)
+    unique: bool | None = Field(None)
+    minimum: int | float | None = Field(None)
+    maximum: int | float | None = Field(None)
+    minLength: int | None = Field(None)
+    maxLength: int | None = Field(None)
+    pattern: str | None = Field(None)
+    enum: list[Any] | None = Field(None)
+
+
+class ColumnValidator(BaseModel):
+    """Validator for the columns in the Table Schema.
+
+    This class is used to validate the columns in the Table Schema. It is based on the
+    columns defined in the Table Schema specification.
+
+    Attributes:
+        name: The name of the column.
+        title: A nicer human readable label or title for the field.
+        type_: A string specifying the type.
+        format_: A string specifying a format.
+        example: An example value for the field.
+        description: A description for the field.
+        constraints: A dictionary of constraints for the field.
+
+    """
+
+    name: str = Field(..., description="Column name.")
+    title: str | None = Field(
+        None, description="A nicer human readable label or title for the field."
+    )
+    type_: TypeEnum | None = Field(
+        None, alias="type", description="A string specifying the type."
+    )
+    format_: str | None = Field(
+        None, alias="format", description="A string specifying a format."
+    )
+    example: str | None = Field(None, description="An example value for the field.")
+    description: str | None = Field(None, description="A description for the field.")
+    constraints: ConstraintsValidator | None = Field(
+        None, description="A dictionary of constraints for the field."
+    )
+
+    def model_dump(self, *args, **kwargs) -> dict[str, Any]:
+        """Dump the model to a dictionary.
+
+        This method dumps the model to a dictionary. It sets exclude_unset to True and
+        by_alias to True, so that only the attributes that were set are included in the
+        dictionary and their aliases are always used.
+
+        Finally, it converts the attributes that are Enum instances to their values.
+
+        Returns:
+            A dictionary with the model attributes.
+
+        """
+        kwargs["exclude_unset"] = True
+        kwargs["by_alias"] = True
+        output = super().model_dump(*args, **kwargs)
+        for key, value in output.items():
+            if isinstance(value, Enum):
+                output[key] = value.value
+        return output
